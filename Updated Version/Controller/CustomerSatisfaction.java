ec87b1201470cb41f8129dd99daeb991ec44e11e
Abigail Borromeo <129239019+abbeeeeyy@users.noreply.github.com>
2023-06-20 10:01:11 +0800

    Update CustomerSatisfaction.java

--- Updated Version/Controller/CustomerSatisfaction.java
+++ Updated Version/Controller/CustomerSatisfaction.java
@@ -1,15 +1,21 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class CustomerSatisfaction implements Initializable {
 
@@ -30,14 +36,43 @@ public class CustomerSatisfaction implements Initializable {
 
         image3.setVisible(true);
 
-        b1.setOnMouseClicked(event -> {
+        b1.setOnMouseEntered(event -> {
             image1.setVisible(true); image2.setVisible(false); image3.setVisible(false);});
 
-        b2.setOnMouseClicked(event -> {
+        b2.setOnMouseEntered(event -> {
             image2.setVisible(true); image1.setVisible(false); image3.setVisible(false);});
+
+        b1.setOnMouseExited(event -> {
+            image1.setVisible(false); image2.setVisible(false); image3.setVisible(true);
+        });
+
+        b2.setOnMouseExited(event -> {
+            image1.setVisible(false); image2.setVisible(false); image3.setVisible(true);
+        });
         
+    }
+
+    public void satisfied(ActionEvent event) throws IOException {
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/ThankYou.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+    }
+
+    public void unsatisfied (ActionEvent event) throws IOException {
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/ThankYou.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
     }
 
 
         } 
-
